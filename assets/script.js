b4daf760822b4a5abe6df5563f217bc17dd0d461
Turyamureeba-cyber <joshmanx9@gmail.com>
2025-09-11 18:47:53 +0300

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+// assets/script.js
+console.log("Lost & Found Uganda app loaded!");
+
+// Example: confirm before deleting
+function confirmDelete() {
+  return confirm("Are you sure you want to delete this item?");
+}
